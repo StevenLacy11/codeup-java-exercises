646d484411c6b2d51a45bdc76dcf220e67014a24
Steven Lacy <steven.lacy11@gmail.com>
2021-10-21 15:20:24 -0500

    first java file written.

---
+++ src/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("This is my first Java program.");
+        System.out.println();
+        System.out.println("This program was written be Steven Lacy.");
+        System.out.println("End of program.");
+    }	// end method main
+}
