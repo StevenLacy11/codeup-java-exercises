be40df9d7f8d9326a87b50cb7ac3d9958da1a494
Steven Lacy <steven.lacy11@gmail.com>
2021-10-22 12:22:55 -0500

    Java Syntax, Types, and Variables discovered.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,42 @@
+import javax.swing.*;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("This is my first Java program.");
         System.out.println();
-        System.out.print("This program was written be Steven Lacy.");
-        System.out.print("End of program.");
+        System.out.println("This program was written be Steven Lacy.");
+
+
+       int myFavoriteNumber = 26;
+        System.out.println(myFavoriteNumber);
+
+        String myString = ("This is my string.");
+         System.out.println(myString);
+
+
+//        int x = 4;
+//        x = x + 5;
+//
+//        x += 5;
+
+//        int x = 3;
+//        int y = 4;
+//        y = y * x;
+//
+//        y *= x;
+
+//        int x = 10;
+//        int y = 2;
+//        x = x / y;
+//        y = y - x;
+//
+//        x /= y;
+//        y -= x;
+
+
+
+
+
+        System.out.println("End of program.");
     }	// end method main
-}
+} // end class HelloWorld
