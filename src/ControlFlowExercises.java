e990b297655d2f9c24634646f922e3e98d7005fe
Steven Lacy <steven.lacy11@gmail.com>
2021-10-25 15:05:41 -0500

    caught the Flow.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,8 @@
-
+import java.util.Scanner;
 
 public class ControlFlowExercises {
 	public static void main(String[] args) {
-		// declare variables to be used
+
 
 //		int i = 5;
 //
@@ -16,10 +16,8 @@ public class ControlFlowExercises {
 			System.out.println(i);
 
 
-
-
-//		for(long i = 2; i <= 1000000; i*=i)
-//			System.out.println(i);
+		for(long i = 2; i <= 1000000; i*=i)
+			System.out.println(i);
 
 		for (int i=1; i<=100; i++) {
 			if (i%15==0) {
@@ -33,15 +31,79 @@ public class ControlFlowExercises {
 			}
 		}
 
+//		 instantiate an object from the
+//		 Scanner class to enable reading
+//		 in from the keyboard
 
 
+        String choice = "y", message;
+		int a;
+		long startValue, squared, cubed;
 
+		Scanner keyboard1 = new Scanner(System.in);
 
+        while (choice.equals("y")) {
 
+            // gets the input from the user
+            System.out.println("What number would you like to go up to?");
+			startValue = keyboard1.nextLong();
 
+            System.out.print("Number | Squared |  Cubed");
+            System.out.print("\n------ | ------  | ------");
 
+            for (a = 1; a <= startValue; a++) {
+                squared = (long) Math.pow(a, 2);
+                cubed = (long) Math.pow(a, 3);
 
+                 message = "\n" + a + "      | " + squared + "      |  " + cubed;
 
+                System.out.print(message);
+            }
 
-	}
-}
+			//// sees if the user wants to continue
+            System.out.print("\nContinue? (y/n): ");
+            choice = keyboard1.next();
+            System.out.println();
+
+            if (choice.equals("n")) {
+				System.out.println("\nThis program was written by Steven Lacy.");
+				System.out.println("End of program.");
+            }
+        }
+
+		long userGrade;
+
+		Scanner keyboard2 = new Scanner(System.in);
+
+		System.out.print("Next one? (y/n): ");
+		choice = keyboard2.next();
+
+		boolean confirmation = choice.equals("y");
+
+		if (confirmation) {
+			System.out.print("Input a numerical grade from 0 to 100\n");
+			userGrade = keyboard2.nextLong();
+
+			if (userGrade >= 88) {
+				System.out.println("A");
+			}
+			else if (userGrade >= 80) {
+				System.out.println("B");
+			}
+			else if (userGrade >= 67) {
+				System.out.println("C");
+			}
+			else if (userGrade >= 60) {
+				System.out.println("D");
+			}
+			else  {
+				System.out.println("F");
+			}
+			System.out.println("\nThis program was written by Steven Lacy.");
+			System.out.println("End of program.");
+		}
+
+	}//end method main
+
+}    //end NumberOperations
+
