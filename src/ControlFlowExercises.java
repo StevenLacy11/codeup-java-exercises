c52312714b81686aa6133bc4016b931c40f93421
Steven Lacy <steven.lacy11@gmail.com>
2021-10-25 11:37:37 -0500

    refactored using for loops

---
+++ src/ControlFlowExercises.java
@@ -0,0 +1,35 @@
+
+
+public class ControlFlowExercises {
+	public static void main(String[] args) {
+		// declare variables to be used
+
+//		int i = 5;
+//
+//		while (i <= 15){
+//			System.out.println(i);
+//			i++;
+//		}
+
+
+		for(int i = 100; i >= -10; i -= 5)
+			System.out.println(i);
+
+
+
+
+//		for(long i = 2; i <= 1000000; i*=i)
+//			System.out.println(i);
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
