c3e4ced224a886d2d534ccec6693c27092c35183
Steven Lacy <steven.lacy11@gmail.com>
2021-11-02 12:25:54 -0500

    lecture Stuff about Inheritance and Polymorphism

---
+++ src/Employee.java
@@ -0,0 +1,33 @@
+public class Employee extends Person {
+
+
+	public Employee(String employeeName) {
+		super(employeeName); // calling the constructor method from Person - Person(employeeName)
+	}
+
+	public int getAge() {
+		return this.age;
+	}
+
+	public void sayHello(){
+		System.out.println("Welcome to MegaCorp - how may I help you?");
+	}
+
+	public void doWork(){
+		System.out.println("Work, Work, Work");
+	}
+
+	public static void main(String[] args) {
+
+		Person unemployedSteven = new Person("Stephen");
+		unemployedSteven.sayHello();
+
+		Employee steven = new Employee("Steven");
+		steven.sayHello();
+		System.out.println("steven.name = " + steven.getName());
+
+		steven.doWork();
+	}
+
+
+}
--- src/Person.java
+++ src/Person.java
@@ -1,9 +1,18 @@
-
 public class Person{
+
+
 	private String name;
+	protected int age;
+	public Person(){}
+
 
 	public Person(String name){
 		this.name = name;
+	}
+
+	public Person(String name,int age){
+		this.name = name;
+		this.age = age;
 	}
 
 	public String getName(){
