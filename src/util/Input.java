f689ac7977962a2b587ad9befb06a40fff5391bb
Steven Lacy <steven.lacy11@gmail.com>
2021-11-08 13:31:45 -0600

    Had to get correct  code

--- src/util/Input.java
+++ src/util/Input.java
@@ -3,56 +3,89 @@ package util;
 import java.util.Scanner;
 
 public class Input {
-	private final Scanner keyboard = new Scanner(System.in);
+	private Scanner scanner;
 
-	public String getString() {
-		System.out.println();
-		return keyboard.nextLine();
+	// constructor
+	public Input() {
+		this.scanner = new Scanner(System.in);
 	}
 
+	public String getString() {
+		System.out.println("Enter a string.");
+		return this.scanner.nextLine();
+	}
+
+	public String getString(String prompt) {
+		System.out.println(prompt);
+		return this.scanner.nextLine();
+	}
+
+	// The yesNo method should return true if the user enters y, yes, or variants thereof, and false otherwise.
 	public boolean yesNo() {
-		System.out.println("Enter yes or no");
-		String yesNo = keyboard.nextLine();
-		//		System.out.println("Your boolean is " + result);
-		return yesNo.equalsIgnoreCase("yes") || yesNo.equalsIgnoreCase("y");
+		String userInput = this.scanner.next();
+		return userInput.equalsIgnoreCase("y") || userInput.equalsIgnoreCase("yes");
+	}
+
+	public boolean yesNo(String prompt) {
+		System.out.println(prompt);
+		return yesNo();
 	}
 
 	public int getInt() {
-		System.out.println("Enter an integer: ");
-		return keyboard.nextInt();
+		return this.scanner.nextInt();
 	}
 
+	public int getInt(String prompt) {
+		System.out.println(prompt);
+		return getInt();
+	}
+
+	// getInt(int min, int max) method should keep prompting the user for input until they give an integer within the min and max. The getDouble method should do the same thing, but with decimal numbers.
 	public int getInt(int min, int max) {
-		String prompt = "Enter an integer between " + min + " and " + max;
-		System.out.println("Enter an integer between " + min + " and " + max);
-		int userInput = keyboard.nextInt();
-		while (userInput <= min || userInput >= max) {
-			System.out.println("Enter an integer between " + min + " and " + max);
-			userInput = keyboard.nextInt();
-		}
-		return userInput;
+		int userInt;
+		do {
+			System.out.printf("Please enter a whole number between %d and %d: %n", min, max);
+			userInt = this.scanner.nextInt();
+		} while(userInt > max || userInt < min);
+		return userInt;
+	}
+
+	public int getInt(int min, int max, String prompt) {
+		int userInt;
+		do {
+			System.out.println(prompt);
+			userInt = this.scanner.nextInt();
+		} while(userInt > max || userInt < min);
+		return userInt;
 	}
 
 	public double getDouble() {
-		System.out.println("Enter a number:");
-		return keyboard.nextDouble();
+		return this.scanner.nextDouble();
 	}
 
 	public double getDouble(String prompt) {
 		System.out.println(prompt);
-		return keyboard.nextDouble();
+		return getDouble();
 	}
 
 	public double getDouble(double min, double max) {
-		String prompt = "Enter an integer between " + min + " and " + max;
-		System.out.println("Enter an integer between " + min + " and " + max);
-		double userInput = keyboard.nextDouble();
-		while (userInput <= min || userInput >= max) {
-			System.out.println("Enter andinteger between " + min + " and " + max);
-			userInput = keyboard.nextDouble();
-		}
-		return userInput;
+		double userDouble;
+		do {
+			System.out.printf("Please enter a decimal number between %f and %f: %n", min, max);
+			userDouble = this.scanner.nextDouble();
+		} while (userDouble > max || userDouble < min);
+
+		return userDouble;
 	}
 
+	public double getDouble(double min, double max, String prompt) {
+		double userDouble;
+		do {
+			System.out.println(prompt);
+			userDouble = this.scanner.nextDouble();
+		} while (userDouble > max || userDouble < min);
+
+		return userDouble;
+	}
 
 }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -1,14 +1,31 @@
+
+
 package util;
 
 public class InputTest {
 	public static void main(String[] args) {
 		Input input = new Input();
-		System.out.println(input.getString());
-		System.out.println(input.yesNo());
-		System.out.println(input.getInt(20, 45));
-		System.out.println(input.getInt());
-		System.out.println(input.getDouble(1.99, 99.99));
-		System.out.println(input.getDouble());
+
+		String myString = input.getString();
+		System.out.println("myString = " + myString);
+
+		System.out.println("Do you like pizza?");
+		boolean likePizza = input.yesNo();
+		System.out.println("likePizza = " + likePizza);
+
+		System.out.println("Please enter a whole number.");
+		int myInt = input.getInt();
+		System.out.println("myInt = " + myInt);
+
+		int myInt2 = input.getInt(1, 100);
+		System.out.println("myInt2 = " + myInt2);
+
+		System.out.println("Please enter a decimal number.");
+		double myDouble = input.getDouble();
+		System.out.println("myDouble = " + myDouble);
+
+		boolean isContinue = input.yesNo("Would you like to continue?");
+		System.out.println("isContinue = " + isContinue);
 
 	}
 }
